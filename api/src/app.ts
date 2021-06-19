8d95e8d58ddce13cff1cc76e5f4f532904a81c9d
azharalifauzi <azharalifauzi@gmail.com>
2021-06-19 11:29:10 +0700

    add default error handling

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,24 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import { defaultErrorHandler } from './controllers/error-handler';
 
 const app = express();
 
 app.use(json({ limit: '10kb' }));
+
+// Welcome Page
+app.get('/api', (_req, res) => {
+  res.send('<h1>Welcome to Null River API</h1>');
+});
+
+// Error Handler
+app.all('*', async (_, res) => {
+  res.status(404).send({
+    message: 'Not Found!',
+  });
+});
+
+app.use(defaultErrorHandler);
 
 export default app;
