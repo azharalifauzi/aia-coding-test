af2f13163427f0c464d10faf97da9a048fb2c14c
azharalifauzi <azharalifauzi@gmail.com>
2021-06-19 16:55:03 +0700

    add ownername and views

--- api/src/controllers/flickr-controller.ts
+++ api/src/controllers/flickr-controller.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 export const getPhotos = async (req: Request, res: Response) => {
   const { limit = 10, search, page = 1 } = req.query;
 
-  let flickrUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&privacy_filter=1&media=photos&per_page=${limit}&page=${page}&format=json&nojsoncallback=1`;
+  let flickrUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&privacy_filter=1&media=photos&per_page=${limit}&page=${page}&format=json&nojsoncallback=1&extras=owner_name,views`;
 
   if (search) flickrUrl = `${flickrUrl}&tags=${search}`;
 
@@ -19,11 +19,16 @@ export const getPhotos = async (req: Request, res: Response) => {
       total: flickrData.photos.total,
       page: flickrData.photos.page,
       length: flickrData.photos.perpage,
-      photos: Array.from(flickrData.photos.photo || [], ({ id, title, server, secret }) => ({
-        id,
-        title,
-        url: `https://live.staticflickr.com/${server}/${id}_${secret}.jpg`,
-      })),
+      photos: Array.from(
+        flickrData.photos.photo || [],
+        ({ id, title, server, secret, ownername, views }) => ({
+          id,
+          title,
+          url: `https://live.staticflickr.com/${server}/${id}_${secret}.jpg`,
+          ownername,
+          views,
+        })
+      ),
     },
   });
 };
