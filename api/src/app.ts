dcf24326a03bfd523181dd822c8216651db0f0ca
azharalifauzi <azharalifauzi@gmail.com>
2021-06-21 11:03:13 +0700

    add security layer for api

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,24 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import rateLimit from 'express-rate-limit';
+import hpp from 'hpp';
+import helmet from 'helmet';
 import { defaultErrorHandler } from './controllers/error-handler';
 import flickrRoute from './routes/flickr-routes';
+
+const limiter = rateLimit({
+  max: 300,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
 
 const app = express();
 
 app.use(json({ limit: '10kb' }));
+app.use(helmet());
+app.use('/api/', limiter);
+app.use(hpp());
 
 app.use('/api/v1/flickr', flickrRoute);
 
